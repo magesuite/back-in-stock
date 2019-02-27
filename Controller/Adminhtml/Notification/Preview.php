33a0695c646af6b9e6492b8de0ecc16d125fb8f8
Blazej Doleska <blazej.doleska@creativestyle.pl>
2019-02-27 09:25:30 +0100

    added preview button functionality

---
+++ Controller/Adminhtml/Notification/Preview.php
@@ -0,0 +1,63 @@
+<?php
+namespace MageSuite\BackInStock\Controller\Adminhtml\Notification;
+
+class Preview extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Magento\Framework\Controller\Result\JsonFactory
+     */
+    protected $resultJsonFactory;
+    /**
+     * @var \MageSuite\BackInStock\Service\PreviewNotificationSender
+     */
+    private $previewNotificationSender;
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
+        \MageSuite\BackInStock\Service\PreviewNotificationSender $previewNotificationSender
+    )
+    {
+        parent::__construct($context);
+
+        $this->resultJsonFactory = $resultJsonFactory;
+        $this->previewNotificationSender = $previewNotificationSender;
+    }
+
+    /**
+     * @return \Magento\Framework\Controller\Result\Json
+     */
+    public function execute()
+    {
+        $data = $this->getRequest()->getPostValue();
+
+        /** @var \Magento\Framework\Controller\Result\Json $result */
+        $result = $this->resultJsonFactory->create();
+
+        if ($data) {
+            try {
+                foreach ($data['message'] as $storeId => $message) {
+                    $this->previewNotificationSender->sendPreview($data['preview_email_address'], $storeId, $message);
+                }
+
+                $result->setData([
+                    'success' => true,
+                    'successMessage' => __('Preview emails has been sent to provided email address.')
+                ]);
+            } catch (\Exception $e) {
+
+                $result->setData([
+                    'success' => false,
+                    'errorMessage' => __('Error occured while sending notifications.', $e->getMessage())
+                ]);
+            }
+        }
+
+        return $result;
+    }
+
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed(self::ADMIN_RESOURCE);
+    }
+}
---
+++ Service/PreviewNotificationSender.php
@@ -0,0 +1,21 @@
+<?php
+namespace MageSuite\BackInStock\Service;
+
+class PreviewNotificationSender
+{
+    /**
+     * @var EmailSender
+     */
+    protected $emailSender;
+
+
+    public function __construct(\MageSuite\BackInStock\Service\EmailSender $emailSender)
+    {
+        $this->emailSender = $emailSender;
+    }
+
+    public function sendPreview($email, $storeId, $message)
+    {
+        $this->emailSender->sendMail($email, ['notification_message' => $message], \MageSuite\BackInStock\Api\Data\NotificationInterface::MANUAL_NOTIFICATION_TEMPLATE, $storeId);
+    }
+}
