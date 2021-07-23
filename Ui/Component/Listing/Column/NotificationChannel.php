78e41b0280d7f7dde53a79b1272c480fb49f2294
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2021-07-23 18:32:44 +0200

    Added notification channel to the back in stock list

---
+++ Ui/Component/Listing/Column/NotificationChannel.php
@@ -0,0 +1,16 @@
+<?php
+namespace MageSuite\BackInStock\Ui\Component\Listing\Column;
+
+class NotificationChannel extends \Magento\Ui\Component\Listing\Columns\Column
+{
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                $item['notification_channel'] = ucfirst($item['notification_channel']);
+            }
+        }
+
+        return $dataSource;
+    }
+}
