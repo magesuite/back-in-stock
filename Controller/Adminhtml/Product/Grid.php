54b5ae2a594f9f65e02dbcfa4b39d72dc8e3291c
Piotr Matras <piotr.matras@creativestyle.pl>
2025-12-15 10:57:34 +0100

    feat: HAAR-5076 change adminhtml extend class

--- Controller/Adminhtml/Product/Grid.php
+++ Controller/Adminhtml/Product/Grid.php
@@ -4,14 +4,11 @@ namespace MageSuite\BackInStock\Controller\Adminhtml\Product;
 
 class Grid extends \Magento\Catalog\Controller\Adminhtml\Product
 {
-    protected $resultLayoutFactory;
-
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
         \Magento\Catalog\Controller\Adminhtml\Product\Builder $productBuilder,
-        \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory
+        protected \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory
     ) {
-        $this->resultLayoutFactory = $resultLayoutFactory;
         parent::__construct($context, $productBuilder);
     }
 
