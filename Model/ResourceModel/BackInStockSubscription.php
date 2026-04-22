87ed7a8888cbf7e6e4d871611cf55de036070fe5
Piotr Matras <piotr.matras@creativestyle.pl>
2026-04-22 14:30:37 +0200

    hotfix: Optimize sql query

--- Model/ResourceModel/BackInStockSubscription.php
+++ Model/ResourceModel/BackInStockSubscription.php
@@ -27,13 +27,14 @@ class BackInStockSubscription extends \Magento\Framework\Model\ResourceModel\Db\
 
     public function getSubscriptionsBySkus($skus)
     {
+        $skus = array_map('strval', $skus);
         $tableName = $this->connection->getTableName($this->getMainTable());
         $notificationTableName = $this->connection->getTableName('back_in_stock_notification_queue');
 
         $query = $this->connection
             ->select()
             ->from(['s' => $tableName], 's.*')
-            ->joinLeft(['e' => $this->connection->getTableName('catalog_product_entity')], 's.product_id = e.entity_id', 'e.sku')
+            ->join(['e' => $this->connection->getTableName('catalog_product_entity')], 's.product_id = e.entity_id', 'e.sku')
             ->joinLeft(
                 ['n' => $notificationTableName],
                 's.id = n.subscription_id',
