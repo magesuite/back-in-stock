c41819dac50270c861181b17269d4c54cd781a4a
Zbigniew Kuraś <zbigniew.kuras@creativestyle.pl>
2026-07-24 21:05:36 +0200

    feat: MGS-5785 fix error while notify customer

--- Model/SourceItem/GetSkuListInStockBySkus.php
+++ Model/SourceItem/GetSkuListInStockBySkus.php
@@ -54,9 +54,10 @@ class GetSkuListInStockBySkus
         $skuListInStockList = [];
         foreach ($items as $stockId => $skuList) {
             /** @var \Magento\InventoryIndexer\Indexer\SourceItem\SkuListInStock $skuListInStock */
-            $skuListInStock = $this->skuListInStockFactory->create();
-            $skuListInStock->setStockId((int)$stockId);
-            $skuListInStock->setSkuList($skuList);
+            $skuListInStock = $this->skuListInStockFactory->create([
+                'stockId' => (int) $stockId,
+                'skuList' => $skuList
+            ]);
             $skuListInStockList[] = $skuListInStock;
         }
         return $skuListInStockList;
